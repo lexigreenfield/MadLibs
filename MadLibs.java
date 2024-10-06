c5b0532d87f8ced6201a4d4927bf944c15f1b79f
lexigreenfield <alexisgreenfield@gmail.com>
2024-10-06 16:22:30 -0700

    create each riddle and display original

--- MadLibs.java
+++ MadLibs.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class MadLibs {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String madLib1 = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+        String madLib2 = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        String madLib3 = "I was late to school today because my <noun> <adverb> stole my <noun>.";
 
+        System.out.println("Original story of MadLib 1: " + madLib1);
+
+        /*
+         * System.out.println("Original story of MadLib 2: " + madLib2);
+         * System.out.println("Original story of MadLib 3: " + madLib3);
+         */
     }
 }
