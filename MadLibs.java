488a4f20aa127772308abd25c71e63de32032730
lexigreenfield <alexisgreenfield@gmail.com>
2024-10-06 16:49:25 -0700

    solved riddle

--- MadLibs.java
+++ MadLibs.java
@@ -15,6 +15,15 @@ public class MadLibs {
         String pluralNounInput = scanner.nextLine();
         System.out.println("Enter a number: ");
         String numberInput = scanner.nextLine();
+        int nounIndex = madLib1.indexOf("<noun>");
+        int pluralNounIndex = madLib1.indexOf("<plural_noun>");
+        int numberIndex = madLib1.indexOf("<number>");
+        int nounIndex2 = madLib1.indexOf(", the");
+        int pluralNounIndex2 = madLib1.indexOf(" and the");
+        int numberIndex2 = madLib1.indexOf(" beasts");
+        System.out.println(madLib1.substring(0, nounIndex) + nounInput + madLib1.substring(nounIndex2, pluralNounIndex)
+                + pluralNounInput + madLib1.substring(pluralNounIndex2, numberIndex) + numberInput
+                + madLib1.substring(numberIndex2));
 
         /*
          * System.out.println("Original story of MadLib 2: " + madLib2);
